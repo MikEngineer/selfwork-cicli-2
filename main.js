aac01c844a38ce9c33e2b524e9a043557439a37b
Michelangelo Ruisi <mruisi96@gmail.com>
2024-12-21 23:28:34 +0100

    terminato selfwork cicli 2

---
+++ main.js
@@ -0,0 +1,19 @@
+let disp = 0;
+let counter = 0;
+
+for (i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {console.log(i);
+    }else {
+        disp += i;
+        counter++;
+    }   
+}
+
+let media = disp / counter;
+
+console.log(`La media dei numeri dispari è: ${media}`);
+
+
+
+
+
